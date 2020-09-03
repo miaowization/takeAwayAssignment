3b88388060b7d8e9d8dcef564fc463dd93a1d334
Aigul Valieva <ar.gabidulina@jet.su>
2020-09-03 14:15:25 +0200

    implemented remote webdriver

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,13 +9,24 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def driver(request):
     options = ChromeOptions()
+    remote = True
+
+    def create_remote_driver():
+        options.add_argument('--no-sandbox')
+        options.add_argument('-remote-debugging-port=9222')
+        options.headless = True
+        command_executor = 'http://localhost:4444/wd/hub'
+        return webdriver.Remote(command_executor, desired_capabilities=options.to_capabilities())
 
     def create_local_driver():
         return webdriver.Chrome(
             executable_path=ChromeDriverManager().install(), options=options)
 
     driver_ = None
-    driver_ = create_local_driver()
+    if remote:
+        driver_ = create_remote_driver()
+    else:
+        driver_ = create_local_driver()
     driver_.set_script_timeout(10)
     driver_.set_page_load_timeout(30)
 
